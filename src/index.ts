fbe72750292bc88aeb3bbc95575728a2607811ad
Radek <rad.szep@gmail.com>
2025-03-04 13:45:20 +0100

    .env

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 
@@ -5,9 +6,10 @@ const app = express();
 const port = 8080;
 
 // Połączenie z MongoDB
-mongoose.connect('mongodb://root:password@db:27017/swiftdb?authSource=admin')
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:27017/${process.env.DB_NAME}?authSource=admin`)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Failed to connect to MongoDB', err));
+
 
 app.get('/', (req, res) => {
     res.send('Hello, Remitly!');
@@ -16,3 +18,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+
+//TODO mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@db:27017/swiftdb?authSource=admin`);
